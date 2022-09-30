1a313801687261e7315d8bf13d65cf9150debe55
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-09-30 20:31:54 +0200

    refacto strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,36 +1,43 @@
 #include "libft.h"
 
-char *ft_strjoin(char const *s1, char const *s2)
+int ft_strlen(const char *s)
 {
-  char *res;
-  int len1, len2, i, y;
-
-  len1 = 0;
-  len2 = 0;
-
-  if (!s1 || !s2)
-		return (NULL);
-  while (s1[len1])
-    len1++;
-  while (s2[len2])
-    len2++;
-  res = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-  if (!res)
-    return (NULL);
+  unsigned int i;
   i = 0;
-  y = 0;
-  while (s1[i])
-  {
-    res[i] = s1[i];
+
+  while(s[i])
     i++;
-  }
-  while (s2[y])
+  return (i);
+}
+
+static char *fill_join(char const *s, char *tmp, unsigned int i)
+{
+  unsigned int y;
+  y = 0;
+  while(s[y])
   {
-    res[i] = s2[y];
+    tmp[i] = s[y];
     i++;
     y++;
   }
-  res[i] = 0;
+  return (tmp);
+}
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+  char *res;
+  unsigned int i;
+  int size;
+
+  if (!s1 || !s2)
+		return (NULL);
+  size = ft_strlen(s1) + ft_strlen(s2);
+  res = (char *)malloc(sizeof(char) * (size + 1));
+  if (!res)
+    return (NULL);
+  i = 0;
+  res = fill_join(s1, res, i);
+  res = fill_join(s2, res, ft_strlen(s1));
   return (res);
 }
 
