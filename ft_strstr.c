0e4cee502881727d7dc55a2dcfcfb2a14b0cc706
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-10-03 01:53:20 +0200

    correc ft_strstr

--- ft_strstr.c
+++ ft_strstr.c
@@ -1,20 +1,20 @@
 #include "libft.h"
 
-char *ft_strstr(char *str, const char *to_find)
+char *ft_strstr(const char *str, const char *to_find)
 {
   unsigned i;
   unsigned j;
 
   i = 0;
   if (to_find[0] == '\0')
-		return (str);
+		return ((char *)str);
   while (str[i])
   {
     j = 0;
     while (str[i + j] == to_find[j])
     {
       if(to_find[j + 1] == 0)
-        return (str + i);
+        return ((char *)str + i);
       j++;
     }
     i++;
