06b380969a365b8e29eb1ac0c16150a1a53d3ba7
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-10-03 01:35:57 +0200

    Correc ft_strsub

--- ft_strsub.c
+++ ft_strsub.c
@@ -1,29 +1,5 @@
 #include "libft.h"
 
-void *ft_memset(void *b, int c, size_t len)
-{
-  size_t i;
-  i = 0;
-
-  while (i < len)
-  {
-    ((unsigned char *)b)[i] = (unsigned char)c;
-    i++;
-  }
-  return (b);
-
-}
-
-void  *ft_memalloc(size_t size)
-{
-  unsigned char *ptr;
-  ptr = malloc(size);
-  if (!ptr)
-    return (NULL);
-  ft_memset(ptr, 0, size);
-  return (ptr);
-}
-
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
   char *res;
