0160756980b07ff06a389f9339732f445568a389
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-10-02 19:00:31 +0200

    ft_putnbr

---
+++ ft_putnbr.c
@@ -0,0 +1,33 @@
+#include "libft.h"
+
+static void ft_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+  long nb;
+
+  nb = n;
+  if (nb > 2147483647 || nb < -2147483648)
+        return ;
+  if (nb < 0)
+  {
+    nb *= - 1;
+    ft_putchar('-');
+  }
+  if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar((nb % 10) + '0');
+	}
+	else
+		ft_putchar(nb + '0');
+}
+
+int main(int ac, char **av)
+{
+  (void)ac;
+  ft_putnbr(atoi(av[1]));
+}
