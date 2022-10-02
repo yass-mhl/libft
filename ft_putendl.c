9fc0a3134951c68d4b2b4de00ead3e150f054a5f
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-10-02 16:00:44 +0200

    ft_putendl

---
+++ ft_putendl.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void ft_putendl(char const *s)
+{
+
+  if (!s)
+    return ;
+  while(*s)
+    write(1, s++, 1);
+  write(1, "\n", 1);
+}
+
+int main(int ac, char **av)
+{
+  (void)ac;
+  ft_putendl(av[1]);
+}
