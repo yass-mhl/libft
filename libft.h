e5f3e07e2a30542026d648b6cd10356503c478eb
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-10-03 01:07:10 +0200

    Makefile V1

--- libft.h
+++ libft.h
@@ -12,7 +12,7 @@
 // int ft_isascii(int c);
 // int ft_isdigit(int c);
 // int ft_isprint(int c);
-// void * ft_memset(void *b, int c, size_t len);
+void * ft_memset(void *b, int c, size_t len);
 // char *ft_itoa(int n);
 // void *ft_memccpy(void *dest, const void *src, int c, size_t n);
 // void *ft_memchr(const void *s, int c, size_t n);
@@ -54,7 +54,7 @@
 // char *ft_strtrim(char const *s);
 // int ft_tolower(int c);
 // int ft_toupper(int c);
-void *ft_memalloc(size_t size);
+// void *ft_memalloc(size_t size);
 // char *ft_strjoin(char const *s1, char const *s2);
 // char *ft_strstr(const char *str, const char *to_find);
 // int ft_isupper(int c);
