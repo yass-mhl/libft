a389c33cacf811d2a38b8fcfff0f7c463efb2d41
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-10-31 14:30:05 +0100

    resolve problem strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,33 +1,35 @@
 #include "libft.h"
 
-static char *fill_join(char const *s, char *tmp, unsigned int i)
-{
-  unsigned int y;
-  y = 0;
-  while(s[y])
-  {
-    tmp[i] = s[y];
-    i++;
-    y++;
-  }
-  return (tmp);
-}
-
 char *ft_strjoin(char const *s1, char const *s2)
 {
-  char *res;
-  unsigned int i;
-  int size;
+	char *res;
+	int i;
+	int len1;
+	int len2;
+	int j;
 
-  if (!s1 || !s2)
+	len1 = 0;
+	while (s1[len1] != '\0')
+		len1++;
+	len2 = 0;
+	while (s2[len2] != '\0')
+		len2++;
+	res = (char *)malloc(sizeof(*res) * (len1 + len2 + 1));
+	if (!res)
 		return (NULL);
-  size = ft_strlen(s1) + ft_strlen(s2);
-  res = (char *)malloc(sizeof(char) * (size + 1));
-  if (!res)
-    return (NULL);
-  i = 0;
-  res = fill_join(s1, res, i);
-  res = fill_join(s2, res, ft_strlen(s1));
-  res[size + 1] = 0;
-  return (res);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		res[i] = s2[j];
+		i++;
+		j++;
+	}
+	res[i] = '\0';
+	return (res);
 }
